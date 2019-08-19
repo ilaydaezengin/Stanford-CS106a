7012dae45ad3e178dcb9fc1ccbf4812823543ef2
Ilayda Zengin <izengin14@ku.edu.tr>
2019-08-19 23:57:29 +0300

    Add files via upload

--- Comp130_HW4_Sum19/src/Comp130_HW4_Sum19.java
+++ Comp130_HW4_Sum19/src/Comp130_HW4_Sum19.java
@@ -1,4 +1,4 @@
-/* Student Name:
+/* Student Name: Ilayda Ezgi Zengin,50302
  * The program allows the user to create tree(s), ornament(s), 
  * present(s) with different size and color to canvas 
  * by clicking the mouse. 
@@ -71,6 +71,31 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
 	
 	// Additional instance variable
 	// Your code starts here
+	JButton createTreeButton;
+	JButton createPresentButton;
+	JButton calculateSpendingButton;
+	JButton clearButton;
+	JCheckBox checkbox;
+	JSlider slider;
+	JComboBox<String> orm_shape;
+	JComboBox<String> orm_color;
+	JComboBox<String> orm_list; 
+	DoubleField orm_spending;
+	DoubleField tree_spending;
+	DoubleField present_spending;
+	DoubleField total_spending;
+	int tree_count = 0;
+	int present_count = 0;
+	int sm_circ_orn_count = 0;
+	int med_circ_orn_count = 0;
+	int lrg_circ_orn_count = 0;
+	int sm_oval_orn_count = 0;
+	int med_oval_orn_count = 0;
+	int lrg_oval_orn_count = 0;
+	int sm_star_orn_count = 0;
+	int med_star_orn_count = 0;
+	int lrg_star_orn_count = 0;
+	
 	// Your code ends here
 	
 	public void run() {
@@ -102,37 +127,36 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
     public void init(){
 		// Your code starts here
     	
-    		JButton createTreeButton = new JButton("Create Tree");
-    		JButton createPresentButton = new JButton("Create Present");
-    		JButton calculateSpendingButton = new JButton("Calculate Spending");
-    		JButton clearButton = new JButton("Clear");
+    		createTreeButton = new JButton("Create Tree");
+    		createPresentButton = new JButton("Create Present");
+    		calculateSpendingButton = new JButton("Calculate Spending");
+    		clearButton = new JButton("Clear");
     		add(createTreeButton,SOUTH);
     		add(createPresentButton,SOUTH);
     		add(clearButton,SOUTH);
     		
-    		JCheckBox checkbox = new JCheckBox("Filled");
+    		checkbox = new JCheckBox("Filled");
     		add(checkbox,NORTH);
-    		JSlider slider = new JSlider(MIN_SLIDER_SIZE,MAX_SLIDER_SIZE,INITIAL_SLIDER_SIZE);
+    		slider = new JSlider(MIN_SLIDER_SIZE,MAX_SLIDER_SIZE,INITIAL_SLIDER_SIZE);
     		add(new JLabel("Ornament Size - "+ MIN_SLIDER_SIZE +""),NORTH);
     		add(slider,NORTH);
     		add(new JLabel(""+ MAX_SLIDER_SIZE +" "),NORTH);
-    		JComboBox orm_shape = new JComboBox();
+    		orm_shape = new JComboBox<String>();
     		orm_shape.addItem(ornaments[0]);
     		orm_shape.addItem(ornaments[1]);
     		orm_shape.addItem(ornaments[2]);
     		orm_shape.setEditable(false);
     		add(orm_shape,NORTH);
 
-    		JComboBox orm_color = new JComboBox();
+    		orm_color = new JComboBox<String>();
     		orm_color.addItem("White");
     		orm_color.addItem("Red");
     		orm_color.addItem("Yellow");
     		orm_color.addItem("Blue");
     		orm_color.setEditable(false);
-    		orm_color.setSelectedItem("White");
     		add(orm_color,NORTH);
     		
-    		JComboBox orm_list = new JComboBox();
+    		orm_list = new JComboBox<String>();
     		createStudentList();
     		for(int i = 0; i < studentList.size(); i++){
     			orm_list.addItem(studentList.get(i));
@@ -142,22 +166,22 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
     		
     		JLabel l1 = new JLabel("Ornament Spending");
     		add(l1,EAST);
-    		DoubleField orm_spending = new DoubleField(0.0);
+    		orm_spending = new DoubleField(0.0);
     		add(orm_spending,EAST);
     		
     		JLabel l2 = new JLabel("Tree Spending");
     		add(l2,EAST);
-    		DoubleField tree_spending = new DoubleField(0.0);
+    		tree_spending = new DoubleField(0.0);
     		add(tree_spending,EAST);
     		
     		JLabel l3 = new JLabel("Present Spending");
     		add(l3,EAST);
-    		DoubleField present_spending = new DoubleField(0.0);
+    		present_spending = new DoubleField(0.0);
     		add(present_spending,EAST);
     		
     		JLabel l4 = new JLabel("Total Spending");
     		add(l4,EAST);
-    		DoubleField total_spending = new DoubleField(0.0);
+    		total_spending = new DoubleField(0.0);
     		add(total_spending,EAST);
     		add(calculateSpendingButton,EAST);
     		addMusicAndBackground();
@@ -171,11 +195,12 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
      */
    public void mouseClicked(MouseEvent e) {	
 	    	// Your code starts here
-    	
+    	createOrnament(e);	
+    	}
 	    	// Your code ends here
     		
 		
-	}
+	
     /**
      * The method to handle the action events.
      * @param ActionEvent - the event created when an action happens
@@ -185,13 +210,20 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
     	String s = e.getActionCommand();
     		if(s.equals("Create Tree")){
     			createTree();
+    			tree_count ++;
     		} else if(s.equals("Create Present")){
     			createPresent();
+    			present_count ++;
     		} else if(s.equals("Clear")){
     			removeAll();
-    			//////////////SPENDING SHOULD ALSO BE CLEANED
+    			total_spending.setValue(0.0);
+    			orm_spending.setValue(0.0);
+    			present_spending.setValue(0.0);
+    			tree_spending.setValue(0.0);
     		} else if(s.equals("Calcuate Spending")){
     			createPrice();
+    			
+    			
     			
     		}
     		}
@@ -202,18 +234,33 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
    
     private void createOrnament(MouseEvent e) {
     		// Your code starts here
-    	if(this.getGCanvas().getAccessibleContext() != null){
-    		if(e.equals("Circle")){
-    			createCircleOrnament(e);
-    		} else if (e.equals("Oval")){
-    			createOvalOrnament(e);
-    		} else if (e.equals("Star")){
-    			createStarOrnament(e);
+    		String name = (String) orm_shape.getSelectedItem();
+    		if(name.equals("Circle")){
+    			GOval circ = createCircleOrnament(e);
+    			if(checkbox.isSelected()){
+    				circ.setFilled(true);
+    				circ.setFillColor(getOrnamentColor());
+    			}
+    			add(circ,e.getX(),e.getY());
+    		} else if (name.equals("Oval")){
+    			GOval circ = createOvalOrnament(e);
+    			if(checkbox.isSelected()){
+    				circ.setFilled(true);
+    				circ.setFillColor(getOrnamentColor());
+    			}
+    			add(circ,e.getX(),e.getY());
+    		} else if (name.equals("Star")){
+    			GPolygon circ = createStarOrnament(e);
+    			if(checkbox.isSelected()){
+    				circ.setFilled(true);
+    				circ.setFillColor(getOrnamentColor());
+    			}
+    			add(circ,e.getX(),e.getY());
     		}
     	} 
 		
     		// Your code ends here
-	}
+	
     private void createPresent() {
     		
     		// Your code starts here
@@ -224,20 +271,35 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
     private Color getOrnamentColor() {
 		// Return the color chosen by the user
 		// Your code starts here
-		
+    	String name = (String) orm_color.getSelectedItem();
+
+		if (name.equals("Red"))
+			return Color.RED;
+
+		if (name.equals("Yellow"))
+			return Color.YELLOW;
+
+		if (name.equals("Blue"))
+			return Color.BLUE;
 		// Your code ends here
-		return Color.RED;
+		return Color.WHITE;
 	}
+		// Your code ends here
+		
     private GOval createCircleOrnament(MouseEvent e) {
     		// Your code starts here
-    	if(e.equals("Small")){
+    	int size = getOrnamentSize();
+    	if(size == 0){
 		GOval circ = new GOval(e.getX(),e.getY(),2*SMALL_CIRCLE_ORNAMENT_SIZE,2*SMALL_CIRCLE_ORNAMENT_SIZE);
+		sm_circ_orn_count++;
 		return circ;
-    	} else if(e.equals("Medium")){
+    	} else if(size == 1){
     	GOval circ = new GOval(e.getX(),e.getY(),2*MEDIUM_CIRCLE_ORNAMENT_SIZE,2*MEDIUM_CIRCLE_ORNAMENT_SIZE);
+    	med_circ_orn_count++;
     	return circ;
     	} else {
     	GOval circ = new GOval(e.getX(),e.getY(),2*LARGE_CIRCLE_ORNAMENT_SIZE,2*LARGE_CIRCLE_ORNAMENT_SIZE);
+    	lrg_circ_orn_count++;
     	return circ;
     	}
     		// Your code ends here
@@ -247,15 +309,18 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
     }
     private GOval createOvalOrnament(MouseEvent e) {
     		// Your code starts here
-    	if(e.equals("Small")){
+    	int size = getOrnamentSize();
+    	if(size == 0){
     		GOval circ = new GOval(e.getX(),e.getY(),SMALL_OVAL_ORNAMENT_WIDTH,SMALL_OVAL_ORNAMENT_HEIGHT);
-    		///////////FİLLED CHECKBOX A NASIL ULAŞICAM ONA GÖRE FİLLED KOYUCAM VE COLOR????
+    		sm_oval_orn_count++;
     		return circ;
-    	} else if(e.equals("Medium")){
+    	} else if(size == 1){
         	GOval circ = new GOval(e.getX(),e.getY(),MEDIUM_OVAL_ORNAMENT_WIDTH,MEDIUM_OVAL_ORNAMENT_HEIGHT);
+        	med_oval_orn_count++;
         	return circ;
     	} else {
-        	GOval circ = new GOval(e.getX(),e.getY(),MEDIUM_OVAL_ORNAMENT_WIDTH,MEDIUM_OVAL_ORNAMENT_HEIGHT);
+        	GOval circ = new GOval(e.getX(),e.getY(),LARGE_OVAL_ORNAMENT_WIDTH,LARGE_OVAL_ORNAMENT_HEIGHT);
+        	lrg_oval_orn_count++;
         	return circ;
     	}
     		// Your code ends here
@@ -264,17 +329,21 @@ public class Comp130_HW4_Sum19 extends GraphicsProgram{
 
     private GPolygon createStarOrnament(MouseEvent e) {
     		// Your code starts here
-    	if(e.equals("Small")){
+    	int size = getOrnamentSize();
+    	if(size == 0){
     		GPolygon star = createStar(SMALL_STAR_ORNAMENT_SIZE);
     		add(star,e.getX(),e.getY());
+    		sm_star_orn_count++;
     		return star;
-    	} else if(e.equals("Medium")){
+    	} else if(size == 1){
     		GPolygon star = createStar(MEDIUM_STAR_ORNAMENT_SIZE);
     		add(star,e.getX(),e.getY());
+    		med_star_orn_count++;
     		return star;
     	} else{
     		GPolygon star = createStar(LARGE_STAR_ORNAMENT_SIZE);
     		add(star,e.getX(),e.getY());
+    		lrg_star_orn_count++;
     		return star;
     	}
     		// Your code ends here
@@ -295,7 +364,10 @@ return 0;
      */
     private int getOrnamentSize() {
 	    	// Your code starts here
-return 0;
+    	int answer = 0;
+    	answer = slider.getValue();
+    	return answer;
+    	
 	    	// Your code ends here
 		
 	}
@@ -303,22 +375,26 @@ return 0;
    private void createTree () {
     		// Your code starts here
 	   GCompound tree = new GCompound();
-	   GPolygon small_tri = new GPolygon(TRUNK_WIDTH,TRUNK_HEIGHT/6);
-	   small_tri.addVertex(0,-TRUNK_HEIGHT/12);
-	   small_tri.addEdge(-TRUNK_WIDTH/4,TRUNK_HEIGHT/6);
-	   small_tri.addEdge(TRUNK_WIDTH/2, 0);
+	   GPolygon small_tri = new GPolygon(3*TRUNK_WIDTH,TRUNK_HEIGHT/2);
+	   small_tri.addVertex(0,-TRUNK_HEIGHT/4);
+	   small_tri.addEdge(-3*TRUNK_WIDTH/2,TRUNK_HEIGHT/4);
+	   small_tri.addEdge(3*TRUNK_WIDTH, 0);
 	   small_tri.setFilled(true);
 	   small_tri.setFillColor(Color.green);
 	   tree.add(small_tri,0,0);
-	   GPolygon big_tri =  new GPolygon(TRUNK_WIDTH,TRUNK_HEIGHT/3);
-	   big_tri.addVertex(0, -TRUNK_HEIGHT/6);
-	   big_tri.addEdge(-TRUNK_WIDTH/2,TRUNK_WIDTH/3);
-	   big_tri.addEdge(TRUNK_WIDTH, 0);
+	   
+	   GPolygon big_tri =  new GPolygon(6*TRUNK_WIDTH,TRUNK_HEIGHT);
+	   big_tri.addVertex(0, -TRUNK_HEIGHT/2);
+	   big_tri.addEdge(-3*TRUNK_WIDTH,TRUNK_HEIGHT);
+	   big_tri.addEdge(6*TRUNK_WIDTH, 0);
 	   big_tri.setFilled(true);
 	   big_tri.setFillColor(Color.green);
-	   tree.add(big_tri,0,TRUNK_HEIGHT/6);
-	   GRect body = new GRect(TRUNK_WIDTH/4,TRUNK_HEIGHT/2);
-	   tree.add(body,(TRUNK_WIDTH-TRUNK_WIDTH/4)/2,TRUNK_HEIGHT/2);
+	   tree.add(big_tri,0,TRUNK_HEIGHT/2);
+	   
+	   GRect body = new GRect(TRUNK_WIDTH,TRUNK_HEIGHT);
+	   body.setFilled(true);
+	   body.setFillColor(Color.BLACK);
+	   tree.add(body,5*TRUNK_WIDTH/2,3*TRUNK_HEIGHT/2);
 	   
 	   int y = TREE_Y_COORDINATE;
 	   int x = rgen.nextInt(0,getWidth());
@@ -328,7 +404,19 @@ return 0;
     }
     private void createPrice() {
 		// Your code starts here
-		
+    	double tree_spending =  tree_count * TREE_PRICE;
+    	double present_spending = present_count * PRESENT_PRICE;
+    	double orn_spending = sm_circ_orn_count * SMALL_CIRCLE_ORNAMENT_PRICE +
+				med_circ_orn_count * MEDIUM_CIRCLE_ORNAMENT_PRICE +
+				lrg_circ_orn_count * LARGE_CIRCLE_ORNAMENT_PRICE +
+				sm_oval_orn_count *  SMALL_OVAL_ORNAMENT_PRICE +
+				med_oval_orn_count * MEDIUM_OVAL_ORNAMENT_PRICE +
+				lrg_oval_orn_count * LARGE_OVAL_ORNAMENT_PRICE +
+				sm_star_orn_count * SMALL_STAR_ORNAMENT_PRICE +
+				med_star_orn_count * MEDIUM_STAR_ORNAMENT_PRICE +
+				lrg_star_orn_count * LARGE_STAR_ORNAMENT_PRICE;
+		double price = tree_spending + present_spending + orn_spending;
+				
 		// Your code ends here
    }
    private GPolygon createStar(int size) {  
